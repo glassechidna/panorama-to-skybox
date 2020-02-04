b573a0777e52e5f6e67c731250c92adfe6744b14
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2020-02-04 16:30:18 +1100

    Stream zip response and use 1024x1024 as the default face image size

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import zipfile
 from io import BytesIO
 
 from PIL import Image
-from flask import Flask, flash, redirect, request, render_template, send_file
+from flask import Flask, Response, flash, redirect, request, render_template, stream_with_context
 from flask_cors import CORS
 
 from panorama import populate_face, CubeFace
@@ -29,7 +29,7 @@ if 'DYNO' in os.environ:
     app.logger.addHandler(logging.StreamHandler(sys.stdout))
     app.logger.setLevel(logging.ERROR)
 
-FACE_SIZE = int(os.environ.get('FACE_SIZE') or '512')
+FACE_SIZE = int(os.environ.get('FACE_SIZE') or '1024')
 
 @app.route('/')
 @app.route('/index')
@@ -60,18 +60,26 @@ def convert():
 
     face_image = Image.new('RGB', (FACE_SIZE, FACE_SIZE), 'black')
 
-    zip_buffer = BytesIO()
-    with zipfile.ZipFile(zip_buffer, "w") as zip:
-        for face in CubeFace:
-            populate_face(panorama_image, face_image, face)
-            face_filename = base_name + "_" + face.name.lower() + "." + extension
+    def generate_zip():
+        with BytesIO() as zip_buffer:
+            with zipfile.ZipFile(zip_buffer, "w") as zip:
+                for face in CubeFace:
+                    zip_buffer_face_offset = zip_buffer.tell()
+                    populate_face(panorama_image, face_image, face)
 
-            with BytesIO() as image_bytes:
-                face_image.save(image_bytes, format=panorama_image.format)
-                image_bytes.seek(0)
-                zip.writestr(face_filename, image_bytes.read(), zipfile.ZIP_STORED)
+                    with BytesIO() as image_bytes:
+                        face_image.save(image_bytes, format=panorama_image.format)
+                        image_bytes.seek(0)
+                        face_filename = base_name + "_" + face.name.lower() + "." + extension
+                        zip.writestr(face_filename, image_bytes.read(), zipfile.ZIP_STORED)
 
-        zip_name = base_name + '_' + 'skybox.zip'
+                    zip_buffer.seek(zip_buffer_face_offset)
+                    yield zip_buffer.read()
 
-    zip_buffer.seek(0)
-    return send_file(zip_buffer, as_attachment=True, attachment_filename=zip_name, mimetype='application/zip')
+                zip_buffer_offset = zip_buffer.tell()
+                zip.close()
+                zip_buffer.seek(zip_buffer_offset)
+                yield zip_buffer.read()
+
+    zip_name = base_name.replace('"', '\\"') + '_' + 'skybox.zip'
+    return Response(stream_with_context(generate_zip()), mimetype='application/zip', headers={'Content-Disposition': 'attachment; filename="' + zip_name + '"'})
